61b544be80dc9ff15be3d26d1a14eb6e72e3e945
RossanKheed <nassor.m.majid@gmail.com>
2026-05-29 08:33:04 +0300

    Create hype_motivation.py
    Implement dynamic motivational logic using conditional control flow and iterative loops.

---
+++ hype_motivation.py
@@ -0,0 +1,13 @@
+#Hypes for motivation 
+name=input("What is your name,legend ! . :")
+print(name)
+age=16
+if age >=16:
+    print("YOU ARE THE GREATEST OF ALL TIME")
+else:
+    print("YOU AIN'T EVEN THE LEGEND")
+for  number in range (10):
+    print(name , "SALUTE  🫡 ")
+print(  name, "YOU AIN'T JUST A LEGEND BUT YOU ARE THE GOAT ")
+my_hobby=input("What is your hobby :")
+print("The hobby of legend is ",my_hobby)
