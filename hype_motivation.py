b64290404b1bb21d4eca133d883351738aa8a34d
RossanKheed <nassor.m.majid@gmail.com>
2026-06-14 20:10:08 +0300

    Update hype_motivation.py

--- hype_motivation.py
+++ hype_motivation.py
@@ -1,13 +1,14 @@
 #Hypes for motivation 
 name=input("What is your name,legend ! . :")
 print(name)
-age=16
+age = int(input("What is your age"))
 if age >=16:
     print("YOU ARE THE GREATEST OF ALL TIME")
 else:
-    print("YOU AIN'T EVEN THE LEGEND")
+    print("YOU AIN'T EVEN LEGEND YET")
 for  number in range (10):
     print(name , "SALUTE  🫡 ")
-print(  name, "YOU AIN'T JUST A LEGEND BUT YOU ARE THE GOAT ")
+print(f"{name} YOU AIN'T JUST A LEGEND BUT YOU ARE THE GOAT ")
+
 my_hobby=input("What is your hobby :")
-print("The hobby of legend is ",my_hobby)
+print(f"The hobby of legend is {my_hobby}")
